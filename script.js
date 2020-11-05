be5a1c6193464041642089600d3d460cb22dd016
Zachary Binx <72980919+ZacharyBinx@users.noreply.github.com>
2020-11-04 18:10:42 -0700

    added variables to js

--- script.js
+++ script.js
@@ -2,13 +2,26 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+// added variables for checkboxes
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var upper = "ABCDDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var special = "!@#$%^&*(){}[]+<>/,.|~?";
+  var numerics = "1234567890";
 
   passwordText.value = password;
 
 }
+
+// function getRandomUpper() {
+//     if (upper === "checked class")
+    
+//     return upper[Math.floor(Math.random()*upper.length)];
+    
+// }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
